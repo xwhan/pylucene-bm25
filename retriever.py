bf885c544251cbf041877d25e5d4326d477c77b0
xwhan <xwhan.reg@gmail.com>
2020-12-16 21:37:31 -0800

    fix bm25 tuning bug

--- retriever.py
+++ retriever.py
@@ -16,6 +16,7 @@ from org.apache.lucene.store import NIOFSDirectory
 from org.apache.lucene.queryparser.classic import QueryParser
 from org.apache.lucene.index import DirectoryReader
 from org.apache.lucene.search import IndexSearcher
+from org.apache.lucene.search.similarities import BM25Similarity
 
 # evaluation tools from MDR
 from mdr.retrieval.utils.utils import para_has_answer
@@ -30,6 +31,9 @@ def init():
     PROCESS_TOK = SimpleTokenizer()
     Finalize(PROCESS_TOK, PROCESS_TOK.shutdown, exitpriority=100)
 
+LUCENE_SEARCHER = None
+
+
 
 def get_score(answer_doc, topk=20):
     """Search through all the top docs to see if they have the answer."""
@@ -41,23 +45,23 @@ def get_score(answer_doc, topk=20):
         topkpara_covered.append(int(para_has_answer(answer, p["title"] + " " + p["text"], PROCESS_TOK)))
 
     return {
-        "5": int(np.sum(topkpara_covered[:5]) > 0),
-        "10": int(np.sum(topkpara_covered[:10]) > 0),
+        # "10": int(np.sum(topkpara_covered[:10]) > 0),
         "20": int(np.sum(topkpara_covered[:20]) > 0),
         "50": int(np.sum(topkpara_covered[:50]) > 0),
         "100": int(np.sum(topkpara_covered[:100]) > 0),
-        # "covered": topkpara_covered
+        "500": int(np.sum(topkpara_covered[:500]) > 0),
+        # "1000": int(np.sum(topkpara_covered[:1000]) > 0),
     }
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--index-path', type=str, help='lucene index path', default='/home/xwhan/data/nq-dpr/corpus/lucene_index')
-    parser.add_argument('--qa-data', type=str, help='qa data for evaluation', default='/home/xwhan/data/nq/nq-test.txt')
-    parser.add_argument('--topk', type=int, default=100)
+    parser.add_argument('--qa-data', type=str, help='qa data for evaluation', default='/home/xwhan/data/nq/nq-dev.txt')
+    parser.add_argument('--topk', type=int, default=500)
     args = parser.parse_args()
 
-    qas = [json.loads(line) for line in open(args.qa_data).readlines()]
+    qas = [json.loads(line) for line in open(args.qa_data).readlines()][:1000]
     questions = [_["question"][:-1] if _["question"].endswith("?") else
                  _["question"] for _ in qas]
     answers = [item["answer"] for item in qas]
@@ -68,38 +72,47 @@ if __name__ == '__main__':
     searchDir = NIOFSDirectory(Paths.get(args.index_path))
     searcher = IndexSearcher(DirectoryReader.open(searchDir))
 
-    parser = QueryParser('Context', analyzer)
-    
+    # try tuning the hyperparameters of bm25
+    for k1 in [0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2]:
+        for b in [0.5, 0.6, 0.7, 0.8, 0.9]:
+            
+            print(f"Grid search.... k1: {k1}; b: {b}")
 
-    retrieved = []
-    print("Searching ...")
-    for q in tqdm(questions):
-        query = parser.parse(QueryParser.escape(q))
-        scoreDocs = searcher.search(query, args.topk).scoreDocs
-        topkDocs = []
-        for hit in scoreDocs:
-            doc = searcher.doc(hit.doc)
-            topkDocs.append({
-                "title": doc.get("Title"),
-                "text": doc.get("Context")
-            })
-        retrieved.append(topkDocs)
-    
-    qas_docs = list(zip(questions, answers, retrieved))
+            searcher.setSimilarity(BM25Similarity(k1, b))
 
-    print("Evaluting answer recall ...")
-    get_score_partial = partial(
-        get_score, topk=args.topk)
-    processes = ProcessPool(processes=20, initializer=init
-    )
-    results = processes.map(get_score_partial, qas_docs)
+            parser = QueryParser('Context', analyzer)
 
-    aggregate = defaultdict(list)
-    for r in results:
-        for k, v in r.items():
-            aggregate[k].append(v)
+            retrieved = []
+            print("Searching ...")
+            for q in tqdm(questions):
+                query = parser.parse(QueryParser.escape(q))
+                # print(q, "|", QueryParser.escape(q), "|", query)
+                # import pdb; pdb.set_trace()
+                scoreDocs = searcher.search(query, args.topk).scoreDocs
+                topkDocs = []
+                for hit in scoreDocs:
+                    doc = searcher.doc(hit.doc)
+                    topkDocs.append({
+                        "title": doc.get("Title"),
+                        "text": doc.get("Context")
+                    })
+                retrieved.append(topkDocs)
+            
+            qas_docs = list(zip(questions, answers, retrieved))
 
-    for k in aggregate:
-        results = aggregate[k]
-        print('Top {:3d} Recall for {} QA pairs: {:.2f} ...'.format(
-            int(k), len(results), np.mean(results)))
+            print("Evaluting answer recall ...")
+            get_score_partial = partial(
+                get_score, topk=args.topk)
+            processes = ProcessPool(processes=32, initializer=init
+            )
+            results = processes.map(get_score_partial, qas_docs)
+
+            aggregate = defaultdict(list)
+            for r in results:
+                for k, v in r.items():
+                    aggregate[k].append(v)
+
+            for k in aggregate:
+                results = aggregate[k]
+                print('Top {:3d} Recall for {} QA pairs: {:.4f} ...'.format(
+                    int(k), len(results), np.mean(results)))
